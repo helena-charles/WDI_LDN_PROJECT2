b7e863caba7d0f9bdbd5e3b75b477230f15e77ec
Helena Charles <helsz@hotmail.co.uk>
2018-02-27 12:51:59 +0000

    Fixing bugs with review funciton

--- public/js/app.js
+++ public/js/app.js
@@ -9,14 +9,11 @@ $(document).ready(function(){
   });
 
   // if star{number} is cicked, change the value of the input to {number}
-  const $ratingValue = $('.ratingValue');
-  const $stars = $('[name=rating]');
-
-  $stars.change(function(e) {
-    $ratingValue.val($(e.target).val());
-  });
+  // const $ratingValue = $('.ratingValue');
+  // const $stars = $('[name=rating]');
+  //
+  // $stars.change(function(e) {
+  //   $ratingValue.val($(e.target).val());
+  // });
 
 });
-
-
-//
