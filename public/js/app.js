b3ac99930bd6aa056ea49940ba3440a54487a363
Helena Charles <helsz@hotmail.co.uk>
2018-02-28 16:03:47 +0000

    Editing styles on buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
+  $('form').validate();
+
   $('.my-slick').slick({
-    arrows: true,
-    // slidesToShow: 1,
-    // slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 3000
+    // arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
   });
 
 });
